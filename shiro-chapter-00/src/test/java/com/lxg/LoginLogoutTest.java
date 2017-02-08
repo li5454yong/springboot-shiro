9b7866e013b9cf9a950c19c03c9bf70c8d9667ad
li5454yong <283056051@qq.com>
2017-02-08 15:39:40 +0800

    Initial commit

---
+++ shiro-chapter-00/src/test/java/com/lxg/LoginLogoutTest.java
@@ -0,0 +1,38 @@
+package com.lxg;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by admin on 2017/2/7.
+ */
+public class LoginLogoutTest {
+    @Test
+    public void testHelloworld() {
+
+        Factory<SecurityManager> factory =
+                new IniSecurityManagerFactory("classpath:shiro.ini");
+
+        SecurityManager securityManager = factory.getInstance();
+
+        SecurityUtils.setSecurityManager(securityManager);
+
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+        try {
+            System.out.println(token);
+            subject.login(token);
+        } catch (AuthenticationException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(true, subject.isAuthenticated());
+        subject.logout();
+    }
+}
---
+++ shiro-chapter-01/src/main/java/com/lxg/shiro/realm/MyRealm1.java
@@ -0,0 +1,32 @@
+package com.lxg.shiro.realm;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
+public class MyRealm1 implements Realm {
+    @Override
+    public String getName() {
+        return "myrealm1";
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken token) {
+
+        return token instanceof UsernamePasswordToken;
+    }
+
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws
+            AuthenticationException {
+        String username = (String) token.getPrincipal();
+        String password = new String((char[]) token.getCredentials());
+        if (!"zhang".equals(username)) {
+            throw new UnknownAccountException();
+        }
+        if (!"123".equals(password)) {
+            throw new IncorrectCredentialsException();
+        }
+
+        return new SimpleAuthenticationInfo(username, password, getName());
+    }
+} 
